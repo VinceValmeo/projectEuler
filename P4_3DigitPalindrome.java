1c31b6eb9045893fa247c628588bdc48a94e15df
Vnc <76777885+VinceValmeo@users.noreply.github.com>
2021-07-13 14:45:57 +0800

    Update P4_3DigitPalindrome.java

--- P4_3DigitPalindrome.java
+++ P4_3DigitPalindrome.java
@@ -4,19 +4,14 @@ public class P4_3DigitPalindrome {
         StringBuilder sb = new StringBuilder(str);
         sb.reverse();
         return sb.toString();
-
     }
 
     public static void main(String[] args) {
 
         int ans = 0;
       
-        
-   
-    
         for (int i = 100; i < 1000; i++) {
             for (int j = 100; j < 1000; j++) {
-                
                 
                 int product = i * j;
                 
@@ -24,15 +19,11 @@ public class P4_3DigitPalindrome {
                 String rev = reverseString(stringProd);
                 
                 if (stringProd.equals(rev) && product > ans) {
-                    ans = product;
-                    
-            
-                    
+                    ans = product;  
                 }
             }
         }
         
-
         System.out.println(ans);
         System.out.println("Program Done");
 
